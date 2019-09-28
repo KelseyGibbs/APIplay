288cd19d57c43438e489eb26b73ab6adc7c80ece
KelseyGibbs <gibbs.kelseylee@gmail.com>
2019-09-27 21:44:06 -0500

    updated again

---
+++ client/src/utils/API.js
@@ -0,0 +1,35 @@
+const BASEURL = "https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10";
+const APIKEY = "&key=200559603-3fbad4d84fcb99d1ee9432d2fcd6f2da";
+const TOTAL = (BASEURL + APIKEY);
+
+export default {
+  search: function() {
+
+  fetch(TOTAL)
+  .then(function(response) {
+    console.log("response" + response)
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(JSON.stringify(myJson));
+    return (JSON.stringify(myJson));
+    
+  })
+  },
+
+  searchAll: function(query) {
+    let thisquery = TOTAL + query;
+    fetch(thisquery)
+    .then(function(response) {
+      console.log("response" + response)
+      return response.json();
+    })
+    .then(function(myJson) {
+      console.log(JSON.stringify(myJson));
+      return (JSON.stringify(myJson));
+      
+    })
+    }
+};
+
+
